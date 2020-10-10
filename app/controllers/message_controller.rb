7f5b6899a74fe32df8df2da5fcb9cdd35378edd2
ugorj <92961244+ugorj@users.noreply.github.com>
2020-10-10 12:50:59 +0000

    missing package

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,3 +1,48 @@
 class MessageController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter :cors_prefligh
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+  def index 
+  	@money = params[:money]
+    receive = params[:address]
+  	address = Blockchain::get_address(receive)
+  	@output = address.transactions[0].outputs[0].value
+  	if @output ==  @money.to_f*(10**8)
+      res = {"status" => "success"}
+      respond_to do |format|
+        format.html { render :index }
+        format.js { render :layout => false}
+        format.json { render json: res }    
+      end
+  	else
+      res = {"status" => "error"}
+      respond_to do |format|
+        format.html { render :index }
+        format.js { render :layout => false}
+        format.json { render json: res }    
+      end
+  	end
+
+  end
+
+  def test
+    address = Blockchain::get_address('1E3z7bn4ZeBa47GBBBSfWBFyw1TrXYonxS')
+    @out = address.transactions[0].outputs
+    @in = address.transactions[0].inputs
+  end
+
+  def success
+  end
+
+  def error
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+ 
+  def cors_preflight_check
+    if request.met
