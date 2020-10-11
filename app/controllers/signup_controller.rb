10d3059facd967de375a375401dde3f21fea6048
ugorj <92961244+ugorj@users.noreply.github.com>
2020-10-11 20:07:37 +0000

    add template files

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -6,4 +6,8 @@ class SignupController < ApplicationController
   	@password=params[:password]
   	@confirm_password=params[:confirm_password]
   	@email=params[:email]
-  	i
+  	if @password==@confirm_password
+  		@wallet = Blockchain::create_wallet(@password, 'd469606f-9967-4b10-a695-1765039c010a', email:@email)
+  	end
+  end
+end
