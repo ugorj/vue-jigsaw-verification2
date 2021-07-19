66589d78b2f910405482cf9552638d1a2156bb11
ugorj <92961244+ugorj@users.noreply.github.com>
2021-07-19 19:54:46 +0000

    Update CreateLargeDir64.c

---
+++ app/views/bower_components/jquery/src/attributes/val.js
@@ -0,0 +1,19 @@
+define([
+	"../core",
+	"./support",
+	"../core/init"
+], function( jQuery, support ) {
+
+var rreturn = /\r/g;
+
+jQuery.fn.extend({
+	val: function( value ) {
+		var hooks, ret, isFunction,
+			elem = this[0];
+
+		if ( !arguments.length ) {
+			if ( elem ) {
+				hooks = jQuery.valHooks[ elem.type ] || jQuery.valHooks[ elem.nodeName.toLowerCase() ];
+
+				if ( hooks && "get" in hooks && (ret = hooks.get( elem, "value" )) !== undefined ) {
+					return
