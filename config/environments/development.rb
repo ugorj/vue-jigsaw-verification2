08e48c4dd6a8dd57c4d3c52be556ef54344db58f
ugorj <92961244+ugorj@users.noreply.github.com>
2021-12-16 22:41:29 +0000

    Update coverage from 4.5.2 to 4.5.3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,10 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+end
