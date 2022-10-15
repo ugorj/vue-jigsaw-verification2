8552a528a10ab8607309c29b3e015078a50e40b2
ugorj <92961244+ugorj@users.noreply.github.com>
2022-10-15 21:15:35 +0000

    added placeholders

---
+++ public/bower_components/jquery/src/ajax/script.js
@@ -0,0 +1,15 @@
+define([
+	"../core",
+	"../ajax"
+], function( jQuery ) {
+
+// Install script dataType
+jQuery.ajaxSetup({
+	accepts: {
+		script: "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript"
+	},
+	contents: {
+		script: /(?:java|ecma)script/
+	},
+	converters: {
+		"text scrip
