e461fc7b12817c62aef4a3f19a0913f783410d42
ugorj <92961244+ugorj@users.noreply.github.com>
2022-10-23 06:25:20 +0000

    Start adding tests

---
+++ public/bower_components/jquery/src/ajax/xhr.js
@@ -0,0 +1,16 @@
+define([
+	"../core",
+	"../var/support",
+	"../ajax"
+], function( jQuery, support ) {
+
+jQuery.ajaxSettings.xhr = function() {
+	try {
+		return new XMLHttpRequest();
+	} catch( e ) {}
+};
+
+var xhrId = 0,
+	xhrCallbacks = {},
+	xhrSuccessStatus = {
+		// file protocol always yie
