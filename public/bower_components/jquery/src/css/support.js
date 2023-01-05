ff37713f25b9294e3b012cd0c978802a0a9873f7
ugorj <92961244+ugorj@users.noreply.github.com>
2023-01-05 21:41:43 +0000

    added a file

--- public/bower_components/jquery/src/css/support.js
+++ public/bower_components/jquery/src/css/support.js
@@ -59,4 +59,33 @@ define([
 			},
 			reliableMarginRight: function() {
 				// Support: Android 2.3
-				// Check if div with explicit width and no margin-right i
+				// Check if div with explicit width and no margin-right incorrectly
+				// gets computed margin-right based on width of container. (#3333)
+				// WebKit Bug 13343 - getComputedStyle returns wrong value for margin-right
+				// This support function is only executed once so no memoizing is needed.
+				var ret,
+					marginDiv = div.appendChild( document.createElement( "div" ) );
+
+				// Reset CSS: box-sizing; display; margin; border; padding
+				marginDiv.style.cssText = div.style.cssText =
+					// Support: Firefox<29, Android 2.3
+					// Vendor-prefix box-sizing
+					"-webkit-box-sizing:content-box;-moz-box-sizing:content-box;" +
+					"box-sizing:content-box;display:block;margin:0;border:0;padding:0";
+				marginDiv.style.marginRight = marginDiv.style.width = "0";
+				div.style.width = "1px";
+				docElem.appendChild( container );
+
+				ret = !parseFloat( window.getComputedStyle( marginDiv, null ).marginRight );
+
+				docElem.removeChild( container );
+
+				return ret;
+			}
+		});
+	}
+})();
+
+return support;
+
+});
