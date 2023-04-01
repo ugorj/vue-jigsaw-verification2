ad9039d595723813fed7bf34c964ea4f25f4aa9d
ugorj <92961244+ugorj@users.noreply.github.com>
2023-04-01 17:08:11 +0000

    clean up imports

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class QrcodeControllerTest < ActionController::TestCase
+class SessionsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
---
+++ test/helpers/calculator_helper_test.rb
@@ -0,0 +1,5 @@
+
+require 'test_helper'
+
+class CalculatorHelperTest < ActionView::TestCase
+end
